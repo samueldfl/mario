68be5d25ab43220b0fc99718f41f8ae2b8bb6c66
israelviana <rael232is@gmail.com>
2026-04-29 18:34:03 -0300

    f

--- Scripts/Level1.gd
+++ Scripts/Level1.gd
@@ -1,12 +1,6 @@
 extends Node2D
 
 func _ready() -> void:
-	GameManager.tile_rect($Ground, Vector2(4000, 48))
-	GameManager.tile_rect($Platform1, Vector2(192, 32))
-	GameManager.tile_rect($Platform2, Vector2(192, 32))
-	GameManager.tile_rect($Platform3, Vector2(256, 32))
-	GameManager.tile_rect($Platform4, Vector2(192, 32))
-	GameManager.tile_rect($Platform5, Vector2(256, 32))
 	$Flag.reached.connect(_on_flag_reached)
 	_update_hud()
 
--- Scripts/Level2.gd
+++ Scripts/Level2.gd
@@ -3,12 +3,6 @@ extends Node2D
 var coins_left := 0
 
 func _ready() -> void:
-	GameManager.tile_rect($Ground, Vector2(4000, 48))
-	GameManager.tile_rect($Platform1, Vector2(256, 32))
-	GameManager.tile_rect($Platform2, Vector2(256, 32))
-	GameManager.tile_rect($Platform3, Vector2(256, 32))
-	GameManager.tile_rect($Platform4, Vector2(256, 32))
-	GameManager.tile_rect($Platform5, Vector2(256, 32))
 	for coin in $Coins.get_children():
 		coins_left += 1
 		coin.collected.connect(_on_coin_collected)
